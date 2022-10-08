c8b1f1d03de023c8739e96122eb99c114599c6ee
Alex Hsieh <alexhsieh12@gmail.com>
2022-10-08 03:55:59 -0700

    began the index css and javascript

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var city = ('Los Angeles', 'Denver', 'San Fransisco', 'Houston', 'New York', 'Chicago', 'Miami', 'Seattle')
+var startSearch = document.getElementById('start-search')
+document.addEventListener('click', startSearch)
+
+function startSearch(event) {
+    var ApiKey = 'f06bbb5c970e4ceca2f445002832426b'
+    fetch('')
+}
